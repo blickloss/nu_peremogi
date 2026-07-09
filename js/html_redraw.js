7cde66b9b4a648bb36b3c1f0d142b60b06f6e4c2
blickloss <blickloss@yandex.ru>
2026-07-09 16:42:08 +0500

    Update html_redraw.js

--- js/html_redraw.js
+++ js/html_redraw.js
@@ -48,14 +48,14 @@ HTMLredraw.prototype.gameOver = function() {
 };
 
 HTMLredraw.prototype.gameWin = function() {
-  var msg = this.getMessage('You\'ve Won!');
+  var msg = this.getMessage('Перемога!');
 
   this.messageWrap.show();
   this.messageWrap.appendChild(msg);
 };
 
 HTMLredraw.prototype.getMessage = function(message) {
-  var data = { h3: message, p: 'Коллектуй <b>бiльше</b> грошив' };
+  var data = { h3: message, p: 'Збери <b>більше</b> грошей' };
 
   var wrap = document.createElement('div');
   for (var tag in data) {
@@ -68,45 +68,24 @@ HTMLredraw.prototype.getMessage = function(message) {
 };
 
 HTMLredraw.prototype.resetEggs = function() {
-  // Например, если яйца отображаются как элементы с классом 'egg',
-  // можно удалить их все:
   var eggs = document.querySelectorAll('.egg');
   for (var i = 0; i < eggs.length; i++) {
     eggs[i].parentNode.removeChild(eggs[i]);
   }
-  // Или сбросить позиции в сетке, если они хранятся в данных
 };
 
 HTMLredraw.prototype.updateLossWithBlink = function(lossCount, blinkIndex) {
-  // Используем существующий контейнер #loss
   var container = document.getElementById('loss');
   if (!container) return;
-
-  // Очищаем контейнер
   container.innerHTML = '';
-
-  // Для каждой жизни создаём иконку
   for (var i = 0; i < lossCount; i++) {
     var icon = document.createElement('span');
     icon.className = 'loss-icon';
-
-    // Если это текущая мигающая иконка (последняя потеря)
     if (i === blinkIndex) {
       icon.classList.add('blink');
     }
-
-    // Вставляем картинку – два способа на выбор:
-
-    // СПОСОБ 1: Использовать <img> (простой и надёжный)
     var img = document.createElement('img');
     icon.appendChild(img);
-
-    // СПОСОБ 2: Установить фон через CSS (если предпочитаете)
-    // icon.style.backgroundImage = "url('img/your-icon.png')";
-    // icon.style.backgroundSize = 'contain';
-    // icon.style.backgroundRepeat = 'no-repeat';
-    // icon.style.backgroundPosition = 'center';
-
     container.appendChild(icon);
   }
 };
